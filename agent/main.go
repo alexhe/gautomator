49fe7f3d13d794121e782a0885c0abc166d9522c
Olivier W <olivier.wulveryck@gmail.com>
2015-03-22 09:46:22 +0100

    Added a clientserver.go file.
    The idea is:
    
    You launch the agent on a host with a "manifest" of command to run.
    The agent is :
    - parsing all the commands from the manifest and add the to a stack
    - creating a communication socket
    - starts to process the stack in FIFO mode
    - when there is a request on the socket, it reads it and resturns its
      current working status
    
    If the agent is launched a second time (or many other time), it checks
    for the presence of the socket file and the ability to dial in.
    
    If it can dial in, he pushes the content of its manifest to the stack of
    the listening process.

--- agent/main.go
+++ agent/main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"github.com/owulveryck/flue"
+	"os"
 )
 
 func main() {
-	flue.ParseTopology()
-	flue.ParseNode()
+	//	flue.ParseTopology()
+	//	flue.ParseNode()
+	if len(os.Args) < 2 {
+		log.Println("We are a server...")
+		flue.Server("/tmp/mysocket.sock")
+	} else {
+		log.Println("We are a client...")
+		command := &RemoteCommand{
+			Cmd:        os.Args[1],
+			Args:       os.Args[2:],
+			Stdin:      os.Stdin,
+			Stdout:     os.Stdout,
+			Stderr:     os.Stderr,
+			StatusChan: remoteSender,
+		}
+		flue.Client(RemoteCommand, "/tmp/mysocket.sock")
+	}
+
 }
