eec2603e4331313f17ce9b96a601de129bee5948
Olivier W <olivier.wulveryck@gmail.com>
2015-03-31 20:43:17 +0200

    ...

--- taskparser.go
+++ taskparser.go
@@ -1,8 +1,8 @@
 package flue
 
 import (
-    "log"
 	"github.com/awalterschulze/gographviz"
+	"log"
 )
 
 func AppendTask(slice []*Task, task *Task) []*Task {
@@ -22,47 +22,46 @@ func AppendString(slice []string, task string) []string {
 	return append(slice, task)
 }
 
-
 func (this *TaskGraphStructure) SetStrict(strict bool) {}
 func (this *TaskGraphStructure) SetDir(directed bool)  {}
 func (this *TaskGraphStructure) SetName(name string)   {}
 func (this *TaskGraphStructure) AddPortEdge(src, srcPort, dst, dstPort string, directed bool, attrs map[string]string) {
-    taskDstExists := false // the flag to check if a task already exist
-    taskSrcExists := false // the flag to check if a task already exist
-    for _, aTask := range this.Tasks {
-	if aTask != nil { 
-	    //log.Println("Current task is",aTask.Name)
-	    // If the task exists, add src as a dependency
-	    if aTask.Name == dst {
-		taskDstExists = true
-		aTask.Deps = AppendString(aTask.Deps,src)
-	    }
-	    if aTask.Name == src {
-		taskSrcExists = true
-	    }
-	} 
-    }
-    // If the task does not exists, create it and add it to the structure
-    if taskSrcExists == false {
-	aTask := NewTask()
-	aTask.Name = src
-	this.Tasks = AppendTask(this.Tasks,aTask)
-    }
-    if taskDstExists == false {
-	aTask := NewTask()
-	aTask.Name = dst
-	aTask.Deps = AppendString(aTask.Deps,src)
-	this.Tasks = AppendTask(this.Tasks,aTask)
-    }
+	taskDstExists := false // the flag to check if a task already exist
+	taskSrcExists := false // the flag to check if a task already exist
+	for _, aTask := range this.Tasks {
+		if aTask != nil {
+			//log.Println("Current task is",aTask.Name)
+			// If the task exists, add src as a dependency
+			if aTask.Name == dst {
+				taskDstExists = true
+				aTask.Deps = AppendString(aTask.Deps, src)
+			}
+			if aTask.Name == src {
+				taskSrcExists = true
+			}
+		}
+	}
+	// If the task does not exists, create it and add it to the structure
+	if taskSrcExists == false {
+		aTask := NewTask()
+		aTask.Name = src
+		this.Tasks = AppendTask(this.Tasks, aTask)
+	}
+	if taskDstExists == false {
+		aTask := NewTask()
+		aTask.Name = dst
+		aTask.Deps = AppendString(aTask.Deps, src)
+		this.Tasks = AppendTask(this.Tasks, aTask)
+	}
 }
 func (this *TaskGraphStructure) AddEdge(src, dst string, directed bool, attrs map[string]string) {
 	this.AddPortEdge(src, "", dst, "", directed, attrs)
 }
 func (this *TaskGraphStructure) AddNode(parentGraph string, name string, attrs map[string]string) {
-    log.Printf("parentGraph: %v, name: %v",parentGraph,name)
-    for key, value := range attrs {
-	log.Printf("Arg: %v, Value:%v",key,value)
-    }
+	log.Printf("parentGraph: %v, name: %v", parentGraph, name)
+	for key, value := range attrs {
+		log.Printf("Arg: %v, Value:%v", key, value)
+	}
 
 }
 func (this *TaskGraphStructure) AddAttr(parentGraph string, field, value string) {}
