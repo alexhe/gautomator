479ad207bd2667daf780cbb3e40925b81b21ed9e
Olivier W <olivier.wulveryck@gmail.com>
2015-04-03 17:42:54 +0200

    Added "constructors" for sigmajs types

--- sigmajs.go
+++ sigmajs.go
@@ -3,7 +3,7 @@ package flue
 // This will convert the TaskGraphStructure into a format suitable for sigmajs
 type SigmaNode struct {
 	Id    int    // "id": "1",
-	Labe  string //"label": "Node 1",
+	Label string //"label": "Node 1",
 	Color string //"color": "rgb(90,90,90)",
 	Size  int    //"size": 100,
 	X     int    //"x": 10,
@@ -19,10 +19,38 @@ type SigmaEdge struct {
 }
 
 type SigmaStructure struct {
-	Edges []SigmaEdge
-	Nodes []SigmaNode
+	Nodes []*SigmaNode
+	Edges []*SigmaEdge
+}
+
+func NewSigmaStructure() *SigmaStructure {
+	return &SigmaStructure{
+		make([]*SigmaNode, 2),
+		make([]*SigmaEdge, 1),
+	}
+}
+func NewSigmaEdge() *SigmaEdge {
+	return &SigmaEdge{
+		-1,
+		-1,
+		-1,
+	}
+}
+
+func NewSigmaNode() *SigmaNode {
+	return &SigmaNode{
+		-1,
+		"Default Node",
+		"rgb(90,90,90)",
+		100,
+		0,
+		0,
+		"Def",
+	}
 }
 
 func GetSigmaTaskStructure(taskGraphStructure *TaskGraphStructure) *SigmaStructure {
+	// First parse the taskGraphStructure.Tasks and create the node array
+	return nil
 
 }
