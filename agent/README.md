e54fdd144fc0f2fd08fc7bd952ca0f398ee10a68
Olivier Wulveryck <olivier.wulveryck-ext@socgen.com>
2015-04-10 10:59:55 +0200

    Examples are in the example subdirectory agent has been renamed in gautomator

--- agent/README.md
+++
@@ -1,3 +0,0 @@
-The proper agent to be launched.
-
-The go install command will generate an agent binary.
--- agent/go.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-GOPATH=$PWD/../../../../../
-dot -Tpng example.dot > ../htdocs/static/example.png
-go run main.go example.dot
