b885655b6a225f4000e660dfb602dc4a4355cc49
Olivier W <olivier.wulveryck@gmail.com>
2015-04-10 22:19:58 +0200

    Added a function to display the SVG (dot must be installed)

--- webroutes.go
+++ webroutes.go
@@ -2,7 +2,11 @@ package gautomator
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/gorilla/mux"
 )
@@ -16,6 +20,30 @@ type Route struct {
 
 type Routes []Route
 
+func displaySvg(w http.ResponseWriter, r *http.Request, taskStructure *TaskGraphStructure) {
+	subProcess := exec.Command("dot", "-Tsvg")
+
+	stdin, err := subProcess.StdinPipe()
+	if err != nil {
+		fmt.Println(err) //replace with logger, or anything you want
+	}
+	defer stdin.Close() // the doc says subProcess.Wait will close it, but I'm not sure, so I kept this line
+
+	subProcess.Stdout = w
+	subProcess.Stderr = os.Stderr
+	if err = subProcess.Start(); err != nil { //Use start, not run
+		fmt.Println("An error occured: ", err) //replace with logger, or anything you want
+
+	}
+	io.WriteString(stdin, "digraph G {\n")
+	io.WriteString(stdin, " a->b\n")
+	io.WriteString(stdin, "}\n")
+	// Command was successful
+	stdin.Close()
+	subProcess.Wait()
+
+}
+
 // Courtesy of http://stackoverflow.com/questions/26211954/how-do-i-pass-arguments-to-my-handler
 func showTasks(w http.ResponseWriter, r *http.Request, taskStructure *TaskGraphStructure) {
 	sigmaStructure := GetSigmaStructure(taskStructure)
@@ -27,6 +55,9 @@ func NewRouter(taskStruct *TaskGraphStructure) *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.Headers("Content-Type", "application/json", "X-Requested-With", "XMLHttpRequest")
+	router.Methods("GET").Path("/svg").Name("TaskIndex").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		displaySvg(w, r, taskStruct)
+	})
 	router.Methods("GET").Path("/tasks").Name("TaskIndex").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		showTasks(w, r, taskStruct)
 	})
