c4bab35467f0f914be45bb625ad50cb5cb143ec2
Olivier W <olivier.wulveryck@gmail.com>
2015-04-13 21:43:59 +0200

    Formating

--- nodeparser.go
+++ nodeparser.go
@@ -3,8 +3,8 @@ package gautomator
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"io/ioutil"
+	"log"
 )
 
 func ParseNode(filename *string) TaskDefinition {
@@ -28,20 +28,20 @@ func ParseNode(filename *string) TaskDefinition {
 }
 
 func (this *TaskGraphStructure) InstanciateTaskStructure(taskInstances TaskDefinition) {
-		allSubTasks := make(map[int]*TaskGraphStructure, 0)
-		index := 0
-		for _, taskInstance := range taskInstances {
-			for _, node := range taskInstance.Hosts {
-			    doNotDuplicate := false
-			    for _, task := range this.Tasks {
-				    if task.Origin == taskInstance.Taskname && task.Node == "null" {
+	allSubTasks := make(map[int]*TaskGraphStructure, 0)
+	index := 0
+	for _, taskInstance := range taskInstances {
+		for _, node := range taskInstance.Hosts {
+			doNotDuplicate := false
+			for _, task := range this.Tasks {
+				if task.Origin == taskInstance.Taskname && task.Node == "null" {
 					// Setting the node to node
 					task.Node = node
 					doNotDuplicate = true
-				    }
-			    }
+				}
+			}
 
-			    if doNotDuplicate != true {
+			if doNotDuplicate != true {
 				subTasks := this.GetSubstructure(taskInstance.Taskname)
 				// If there is subtask
 				if subTasks != nil {
@@ -53,41 +53,39 @@ func (this *TaskGraphStructure) InstanciateTaskStructure(taskInstances TaskDefin
 					index += 1
 				} else {
 					for _, task := range this.Tasks {
-					    if task.Name == taskInstance.Taskname {
-						if task.Node == "null" || task.Node == node {
-							task.Node = node
-						} else {
-						    newIds := this.DuplicateTask(taskInstance.Taskname)
-						    for _, newId := range newIds {
-							if newId != -1 {
-							    this.Tasks[newId].Node = node
+						if task.Name == taskInstance.Taskname {
+							if task.Node == "null" || task.Node == node {
+								task.Node = node
+							} else {
+								newIds := this.DuplicateTask(taskInstance.Taskname)
+								for _, newId := range newIds {
+									if newId != -1 {
+										this.Tasks[newId].Node = node
+									}
+								}
 							}
-						    }
 						}
-					    }
 					}
 				}
-			    }
 			}
 		}
+	}
 
-		for _, subTask := range allSubTasks {
-			//subTask.PrintAdjacencyMatrix()
-			this = this.AugmentTaskStructure(subTask)
-		}
-		this.Relink()
-		// Now, for each task, assign module, hosts and co...
-		for _, task := range this.Tasks {
-			if _, ok := taskInstances[task.Name]; ok {
-				if taskInstances[task.Name].Module != "" {
-					task.Module = taskInstances[task.Name].Module
-				}
-				if taskInstances[task.Name].Args != nil {
-					task.Args = taskInstances[task.Name].Args
-				}
+	for _, subTask := range allSubTasks {
+		//subTask.PrintAdjacencyMatrix()
+		this = this.AugmentTaskStructure(subTask)
+	}
+	this.Relink()
+	// Now, for each task, assign module, hosts and co...
+	for _, task := range this.Tasks {
+		if _, ok := taskInstances[task.Name]; ok {
+			if taskInstances[task.Name].Module != "" {
+				task.Module = taskInstances[task.Name].Module
+			}
+			if taskInstances[task.Name].Args != nil {
+				task.Args = taskInstances[task.Name].Args
 			}
 		}
-
-
+	}
 
 }
